8268cf4da849f1c5c64858f81c4039edafaf7b51
zhangjiayi <jiayizhang662@gmail.com>
2025-10-21 13:54:58 +0800

    Update UserGuide for MarkAllAttendanceCommand and Ui

--- src/main/java/seedu/address/model/ReadOnlyAddressBook.java
+++ src/main/java/seedu/address/model/ReadOnlyAddressBook.java
@@ -1,10 +1,10 @@
 package seedu.address.model;
 
 import java.util.List;
+
 import javafx.collections.ObservableList;
 import seedu.address.model.event.Consultation;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.UniquePersonList;
 
 /**
  * Unmodifiable view of an address book
